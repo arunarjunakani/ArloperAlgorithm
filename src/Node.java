556613cb46819712ec16f9cc80461d9486cfaadc
Arun Arjunakani <arunarjunakani@gmail.com>
2016-11-01 23:27:39 -0400

    Added calculateDistance() method

--- src/Node.java
+++ src/Node.java
@@ -6,7 +6,7 @@
  *
  * @author Arun Arjunakani
  * @version 1.0
- * @since 2016-10-24
+ * @since 2016-11-1
  */
 
 public class Node
@@ -16,14 +16,19 @@ public class Node
     private double y;
     private Node n1;
     private Node n2;
-
+    
+    /**
+    * Constuctor with position
+    */
     public Node(double x, double y)
     {
         //Declare variables
         this(x, y, null, null);
     }
 
-
+    /**
+    * Constuctor with position and neighbors
+    */
     public Node(double x, double y, Node n1, Node n2)
     {
         //Declare variables
@@ -35,8 +40,8 @@ public class Node
 
     /**
      * Returns the X coordinate
-     * @param none
-     * @return x
+     * @param null
+     * @return double
      */
     public double getX()
     {
@@ -46,8 +51,8 @@ public class Node
 
     /**
      * Returns the Y coordinate
-     * @param none
-     * @return y
+     * @param null
+     * @return double
      */
     public double getY()
     {
@@ -56,7 +61,7 @@ public class Node
 
     /**
      * Changes the X coordinate
-     * @param x The new x Value
+     * @param double
      * @return null
      */
     public void setX(double x)
@@ -66,7 +71,7 @@ public class Node
 
     /**
      * Changes the Y coordinate
-     * @param y The new y Value
+     * @param double
      * @return null
      */
     public void setY(double y)
@@ -75,13 +80,22 @@ public class Node
     }
 
     /**
-     *
-     * @return
+     * This counts the number of neighbors with a handy 
+     * @param null
+     * @return int
      */
     public int numNeighbors()
     {
-        int neighbors = 0;
-        neighbors = this.n1 == null? this.n2 == null ? 0: 1: 2;
-        return neighbors;
+        return this.n1 == null? this.n2 == null ? 0: 1: 2;
+    }
+    
+    /** 
+     * This calculates the distance between two nodes
+     * @param Node, Node
+     * @return double
+     */
+    public static double calculateDistance(Node n1, Node n2)
+    {
+        return Math.sqrt((n1.getX - n2.getX)*(n1.getX - n2.getX) + (n1.getY - n2.getY)*(n1.getY - n2.getY));
     }
 }
